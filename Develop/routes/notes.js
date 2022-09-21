e387f49feb5def12f4dffcbd0ab6891dbbe2d1ac
Indigo <indigo.crandell@gmail.com>
2022-09-21 14:01:55 -0400

    Created a server. Which is not yet working because I have required non-existent api routes.

---
+++ Develop/routes/notes.js
@@ -0,0 +1,8 @@
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+// const {readFromFile etc.}
+
+// GET route for retrieving all notes
+// notes.get('/notes', (req, res) => {
+    
+// })
---
+++ Develop/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+// const { nameOfMiddleware } = require('./middleware/nameOfMiddleware');
+// TODO: fix index.js/notes.js to get appropriate api data
+const api = require('./routes/index.js');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+//import custom middleware
+//app.use(nameOfMiddleware);
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api); // TODO: see above, fix api
+
+// Make the files in the public folder accessible?
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for notes page
+app.get('/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.listen(PORT, () => 
+   console.log(`App listening at http://localhost:${PORT}`) 
+);
