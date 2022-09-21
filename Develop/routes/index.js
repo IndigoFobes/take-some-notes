71a10fa6f55f98d6d3a542d40bd94d63bc886448
Indigo <indigo.crandell@gmail.com>
2022-09-21 13:09:37 -0400

    Added routes folder with files.

---
+++ Develop/routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+// Require the notes router file
+const notesRouter = require('./notes');
+
+// set express as a variable to use
+const app = express();
+
+// app.use('/tips')
