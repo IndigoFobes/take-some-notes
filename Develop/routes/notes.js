551ac49b6390d6274724c9de0691647fa965e745
Indigo <indigo.crandell@gmail.com>
2022-09-21 15:48:30 -0400

    Wow, the post method is working. I added the post route in the notes.js route file.

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -36,9 +36,8 @@ const readAndAppend = (content, file) => {
 
 // GET route for retrieving all notes
     // Read json from db.json file and return parsed data
-notes.get('/api/notes', (req, res) => {
+notes.get('/', (req, res) => {
     console.info(`${req.method} request received for notes`);
-
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
@@ -48,7 +47,25 @@ notes.get('/api/notes', (req, res) => {
 // });
 
 // POST route for a new note
-//notes.post('/api/notes')
+notes.post('/', (req, res) => {
+    console.log(req.body);
+
+    const { title, text } = req.body;
+
+    // if there is indeed a title and text
+    if (req.body) {
+        const newNote = {
+            title, 
+            text,
+            tip_id: uuidv4(),
+        };
+
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note added successfully!`)
+    } else {
+        res.error('Error adding note.');
+    }
+})
 
 // Export notes router
 module.exports = notes;
