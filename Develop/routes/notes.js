b129b7b3f71b27d9dcc31010518f9c488cd26c54
Indigo <indigo.crandell@gmail.com>
2022-09-21 21:21:19 -0400

    Took out the promisify function. Trying to jsut simply use fs.writefile and fs.readfile. But I am struggling to figure out how to do so without .then.

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -3,49 +3,27 @@ const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 const util = require('util');
 
-// convert callback-based functions to promise-based funcions, using util package
-const readFromFile = util.promisify(fs.readFile);
-
-/**
- * Write data to JSON file
- * @param {string} destination the file I want to write to
- * @param {object} content content I want to write to the file
- * @returns {void} 
- */
-const writeToFile = (destination, content) =>
-fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
-    err ? console.error(err) : console.info(`\nData written to ${destination}`)
-);
-/**
- * Read data from a given file and append content
- * @param {object} content content I want to append
- * @param {string} file path to file I want to save to
- * @returns {void}
- */
-const readAndAppend = (content, file) => {
-    fs.readFile(file, 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-        } else {
-            const parsedData = JSON.parse(data);
-            parsedData.push(content);
-            writeToFile(file, parsedData);
-        }
-    });
-};
-
 // GET route for retrieving all notes
     // Read json from db.json file and return parsed data
 notes.get('/', (req, res) => {
     console.info(`${req.method} request received for notes`);
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) {
+            throw err;
+        } else {
+        res.json(JSON.parse(data));}
+    })
 });
 
 // GET route for a specific note
 notes.get('/:note_id', (req, res) => {
     const noteId = req.params.note_id;
-    readFromFile('./db/db.json')
-    .then((data) => JSON.parse(data))
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) {
+            throw err;
+        } 
+        return JSON.parse(data);
+    })
     .then((json) => {
         const result = json.filter((note) => note.note_id === noteId);
         return result.length > 0
@@ -57,14 +35,22 @@ notes.get('/:note_id', (req, res) => {
 // DELETE route for a specific note
 notes.delete('/:note_id', (req, res) => {
     const noteId = req.params.note_id;
-    readFromFile('./db/db.json')
-    .then((data) => JSON.parse(data))
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) {
+            throw err;
+        }
+        JSON.parse(data);
+    })
     .then((json) => {
         const result = json.filter((note) => note.note_id !== noteId);
 
-        writeToFile('./db/db.json', result);
-
-        res.json(`Note ${noteId} has been deleted.`);
+        //writeToFile('./db/db.json', result);
+        fs.writeFile('./db/db.json', result, (err) => {
+            if (err) {
+                throw err;
+            }
+            res.json(`Note ${noteId} has been deleted.`);
+        })
     });
 });
 
@@ -81,12 +67,22 @@ notes.post('/', (req, res) => {
             text,
             note_id: uuidv4(),
         };
-
-        readAndAppend(newNote, './db/db.json');
-        res.json(`Note added successfully!`)
-    } else {
-        res.error('Error adding note.');
-    }
+        //readAndAppend(newNote, './db/db.json');
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                throw err
+            }
+            JSON.parse(data);
+        })
+        .then((json) => {
+            fs.writeFile('./db/db.json', json, (err) => {
+                if (err) {
+                    console.error('There was an error adding your note!')
+                }
+                res.json(`Note added successfully!`);
+            })
+        })
+    } 
 })
 
 // Export notes router
